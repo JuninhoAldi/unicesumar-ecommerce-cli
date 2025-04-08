915fe96decc35b9d4e28a3a6dd84939590f484cd
Rafael Labegalini <48809390+rafael-labegalini@users.noreply.github.com>
2025-04-08 17:16:06 -0300

    Exercicio de avaliação final

--- scripts/init.sql
+++ scripts/init.sql
@@ -10,3 +10,21 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL,
     password TEXT NOT NULL
 );
+
+-- Table: sales
+CREATE TABLE sales (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    payment_method TEXT NOT NULL,
+    sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(uuid),
+);
+
+-- Table: sale_products
+CREATE TABLE sale_products (
+    sale_id UUID NOT NULL,
+    product_id UUID NOT NULL,
+    PRIMARY KEY (sale_id, product_id),
+    FOREIGN KEY (sale_id) REFERENCES sales(id),
+    FOREIGN KEY (product_id) REFERENCES products(uuid)
+);
