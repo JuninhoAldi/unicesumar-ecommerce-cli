ba81939ac09ded48549d4be5f3fc2d97756af6f0
JuninhoAldi <160974050+JuninhoAldi@users.noreply.github.com>
2025-04-19 17:32:56 -0300

    final

--- scripts/init.sql
+++ scripts/init.sql
@@ -12,16 +12,16 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 -- Table: sales
-CREATE TABLE sales (
+CREATE TABLE IF NOT EXISTS sales (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
     payment_method TEXT NOT NULL,
     sale_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(uuid),
+    FOREIGN KEY (user_id) REFERENCES users(uuid)
 );
 
 -- Table: sale_products
-CREATE TABLE sale_products (
+CREATE TABLE IF NOT EXISTS sale_products (
     sale_id UUID NOT NULL,
     product_id UUID NOT NULL,
     PRIMARY KEY (sale_id, product_id),
